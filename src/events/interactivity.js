e0e3ff7c90f033f2ae316c95699f40c9f59bbf92
dcnguyen07 <130775410+dcnguyen07@users.noreply.github.com>
2024-10-08 14:49:44 +0700

    add interactivity

---
+++ src/events/interactivity.js
@@ -0,0 +1,88 @@
+import { Application, Assets, Sprite, Texture } from "pixi.js";
+
+export class Interactivity{
+    constructor(){
+        this.init();
+    }
+    async init(){
+        const app = new Application();
+        await app.init({ resizeTo: window});
+        document.body.appendChild(app.canvas);
+
+        await Assets.load([
+            'https://pixijs.com/assets/bg_button.jpg',
+            'https://pixijs.com/assets/button.png',
+            'https://pixijs.com/assets/button_down.png',
+            'https://pixijs.com/assets/button_over.png',
+        ]);
+
+        const background = Sprite.from('https://pixijs.com/assets/bg_button.jpg');
+        background.width = app.screen.width;
+        background.height = app.screen.height;
+        app.stage.addChild(background);
+
+        const textureButton = Texture.from('https://pixijs.com/assets/button.png');
+        const textureButtonDown = Texture.from('https://pixijs.com/assets/button_down.png');
+        const textureButtonOver = Texture.from( 'https://pixijs.com/assets/button_over.png');
+
+        const buttons = [];
+        const buttonPositions = [175, 75, 655, 75, 410, 325, 150, 465, 685, 445];
+
+        for (let i = 0; i < 5; i++){
+            const button = new Sprite(textureButton);
+            button.anchor.set(0.5);
+            button.x = buttonPositions[i * 2];
+            button.y = buttonPositions[i * 2 + 1];
+
+            button.eventMode = 'static';
+            button.cursor = 'pointer';
+            button
+            .on('pointerdown', onButtonDown)
+            .on('pointerup', onButtonUp)
+            .on('pointerupoutside', onButtonUp)
+            .on('pointerover', onButtonOver)
+            .on('pointerout', onButtonOut);
+
+            app.stage.addChild(button);
+            buttons.push(button);
+        }
+
+        buttons[0].scale.set(1.2);
+        buttons[2].rotation = Math.PI / 10;
+        buttons[3].scale.set(0.8);
+        buttons[4].scale.set(0.8, 1.2);
+        buttons[4].rotation = Math.PI;
+        
+        function onButtonDown(){
+            this.isdown = true;
+            this.texture = textureButtonDown;
+            this.alpha = 1;
+        }
+
+        function onButtonUp(){
+            this.isdown = false;
+            if(this.isOver){
+                this.texture = textureButtonOver;
+            }
+            else{
+                this.texture = textureButton;
+            }
+        }
+
+        function onButtonOver(){
+            this.isOver = true;
+            if (this.isdown){
+                return;
+            }
+            this.texture = textureButtonOver;
+        }
+
+        function onButtonOut(){
+            this.isOver = false;
+            if(this.isdown){
+                return;
+            }
+            this.texture = textureButton
+        }
+    }
+}
